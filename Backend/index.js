462f10f4c92982b3b49909edcbbf2f0ecd1fdeb7
dhanashreesk <dhanashree.ask22.10.2002@gmail.com>
2024-07-03 22:55:10 +0530

    vercel  deployment

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,12 @@
 // index.js
 const app = require('./app');
+const path = require("path");
 const port = process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+  app.use(express.static(path.resolve(__dirname,"client","build")));
+  res.sendFile(path.resolve(__dirname,"client","build","index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
